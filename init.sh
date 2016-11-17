97d9848b3d5a2fc250788dbc1b67cb7322a5b898
Dmitriy Zabranskiy <d.zabranskiy@semrush.com>
2016-11-17 16:59:32 +0300

    fix it

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
-sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo gunicorn -b 0.0.0.0:8080 hello:application &
 sudo /etc/init.d/gunicorn restart
